a02942aba4b1663f4dcf5345b808b6f99cffcd71
Luca Olivieri <oli.luca93@gmail.com>
2021-07-19 18:18:30 +0200

    Renamed constant

--- go-lisa/src/main/java/it/unive/golisa/util/GoLangUtils.java
+++ go-lisa/src/main/java/it/unive/golisa/util/GoLangUtils.java
@@ -4,19 +4,19 @@ import it.unive.lisa.program.cfg.statement.VariableRef;
 import it.unive.lisa.symbolic.value.Variable;
 
 /**
- * @GoLangUtils contains usefull utility methods and constants for handle GoLang statements
+ * @GoLangUtils contains useful utility methods and constants for handle GoLang statements
  * 
  * @author <a href="mailto:luca.olivieri@univr.it">Luca Olivieri</a>
  */
 public class GoLangUtils {
 
-	public static final String BLANK_IDENTIFIER_NAME = "_";
+	public static final String BLANK_IDENTIFIER = "_";
 	
 	public static boolean isBlankIdentifier(Variable v) {
-		return v.getName().equals(BLANK_IDENTIFIER_NAME);
+		return v.getName().equals(BLANK_IDENTIFIER);
 	}
 	
 	public static boolean refersToBlankIdentifier(VariableRef vref) {
-		return vref.getName().equals(BLANK_IDENTIFIER_NAME);
+		return vref.getName().equals(BLANK_IDENTIFIER);
 	}
 }

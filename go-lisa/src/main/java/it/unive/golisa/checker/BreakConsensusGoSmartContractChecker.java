ad8f9e5200288e6f55d116543df1b6bd0cc5a2b5
Luca Olivieri <oli.luca93@gmail.com>
2021-08-26 10:12:47 +0200

    Reworking break consensus checker.

--- go-lisa/src/main/java/it/unive/golisa/checker/BreakConsensusGoSmartContractChecker.java
+++ go-lisa/src/main/java/it/unive/golisa/checker/BreakConsensusGoSmartContractChecker.java
@@ -1,9 +1,10 @@
-package it.unive.golisa.analysis;
+package it.unive.golisa.checker;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import it.unive.golisa.cfg.expression.unary.GoRange;
 import it.unive.golisa.cfg.statement.GoRoutine;
 import it.unive.golisa.golang.api.signature.FuncGoLangApiSignature;
 import it.unive.golisa.golang.api.signature.GoLangApiSignature;
@@ -19,9 +20,15 @@ import it.unive.lisa.program.cfg.edge.Edge;
 import it.unive.lisa.program.cfg.statement.Statement;
 import it.unive.lisa.program.cfg.statement.UnresolvedCall;
 import it.unive.lisa.program.cfg.statement.VariableRef;
+import it.unive.lisa.program.cfg.statement.Expression;
+import it.unive.golisa.cfg.expression.unary.GoChannelReceive;
+import it.unive.golisa.cfg.expression.unary.GoLength;
+import it.unive.golisa.cfg.expression.binary.GoChannelSend;
+import it.unive.golisa.cfg.expression.binary.GoLess;
 import it.unive.lisa.type.Untyped;
+import it.unive.lisa.type.Type;
 
-public class BreakConsensusChaincodeChecker implements SyntacticCheck {
+public class BreakConsensusGoSmartContractChecker implements SyntacticCheck {
 
 	@Override
 	public void beforeExecution(CheckTool tool) {
@@ -35,8 +42,6 @@ public class BreakConsensusChaincodeChecker implements SyntacticCheck {
 	public boolean visit(CheckTool tool, CFG graph, Statement node) {
 
 		checkIssuesRelatedToGoLangAPI(tool, graph, node);
-		
-		checkIssuesRelatedToHyperLeadgerFabric(tool, graph, node);
 		
 		return true;
 	}
@@ -81,6 +86,8 @@ public class BreakConsensusChaincodeChecker implements SyntacticCheck {
 	private boolean matchOsApi(UnresolvedCall call) {
 
 		return matchAnyPackageSignatures("os", call, true)
+				|| matchAnyPackageSignatures("internal", call, true)
+				|| matchAnyPackageSignatures("syscall", call, true)
 				|| matchAnyPackageSignatures("internal", call, true);
 	}
 
@@ -133,43 +140,10 @@ public class BreakConsensusChaincodeChecker implements SyntacticCheck {
 		
 		return false;
 	}
-
-	private void checkIssuesRelatedToHyperLeadgerFabric(CheckTool tool, CFG graph, Statement node) {
-
-		/*
-		 * Checks taken from paper read by Imran
-		 * A Survey on Hyperledger Fabric Chaincodes Vulnerabilities and Formal Analysis Techniques
-		 */
-		if (node instanceof UnresolvedCall) {
-			if(matchPhantomRead((UnresolvedCall) node))
-				tool.warnOn(node, "Phantom read detected!");
-			if(matchGlobalVariable(node))
-				tool.warnOn(node, "Global variable detected!");
-			if(matchFieldDeclaration(node))
-				tool.warnOn(node, "Unsafe field declaration detected!");
-			if(matchReadWriteSet(node))
-				tool.warnOn(node, "Read-write set detected!");
-		}
-		
-	}
 	
 	
 	
 	
-	/*
-	 * Read-write set. During simulation of a transaction at an endorser, a read-write set is prepared for the transaction.
-	 * The read-set contains a list of unique keys and their committed version numbers that the transaction reads
-	 * during the simulation. The write-set contains a list of unique keys and their new values that the transaction
-	 * writes. Notice that, if the transaction writes a value multiple times for a key, only the last written value is retained.
-	 * Also, if a transaction reads a value for a key, the value in the committed state is returned even if the transaction
-	 * has updated the value for the key before issuing the read. Therefore, in Hyperledger Fabric, read-your-writes
-	 * semantics may create inconsistency among the peers. The following code snippet contain such vulnerability
-	 */
-	private boolean matchReadWriteSet(Statement node) {
-		// TODO 
-		return false;
-	}
-
 	/*
 	 * Maps. Go supports a built-in map type that implements a hash table. The range keyword allows us to iterate
 	 * over maps. However, the order in which the map entries are ranged is not deterministic. Therefore, iterating
@@ -178,21 +152,29 @@ public class BreakConsensusChaincodeChecker implements SyntacticCheck {
 	 * such vulnerability
 	 */
 	private boolean matchMapIterationStatement(Statement node) {
-		//TODO: currenty "range" instruction is not supported
+		if(node instanceof GoRange) {
+			return true;
+			//TODO: type checking/inference needed
+/*			GoRange range = (GoRange) node;
+			for(Expression e1 : range.getParameters()) {
+				if(e1 instanceof GoLess) {
+					GoLess less = (GoLess) e1;
+					for(Expression e2 : less.getParameters()) {
+						if(e2 instanceof GoLength) {
+							GoLength length = (GoLength) e2;
+							Type type = null;
+							for(Expression e3 : length.getParameters()) {
+								type = e3.getStaticType();
+							}
+						}
+					}
+				}
+			}
+*/
+		}
 		return false;
 	}
 
-	/*
-	 * Field declarations. Developers has to define two methods namely: Init() and Invoke(), to implement chaincode interface in Go. The Init() method is used to initialize the chaincode applications. All chaincodes need to
-	 * have an an Init() function. The Invoke() method is used to invoke chaincode functions. When these methods
-	 * are implemented as methods of a structure, developer can also define fields of the structure. The field can be
-	 * accessed and used as global state in these methods. This may create inconsistency because every peer does not
-	 * execute every transaction and therefore, state does not keep identical value among peers.
-	 */
-	private boolean matchFieldDeclaration(Statement node) {
-		// TODO 
-		return false;
-	}
 
 	/*
 	 * Concurrency. Goroutines and channels introduce concurrency into Go chaincodes. The concurrency may
@@ -200,46 +182,13 @@ public class BreakConsensusChaincodeChecker implements SyntacticCheck {
 	 * vulnerability.
 	 */
 	private boolean matchConcurrencyStatement(Statement node) {
-		// TODO : : currently go channel are not supported
-		return node instanceof GoRoutine;
+		return node instanceof GoRoutine || node instanceof GoChannelReceive || node instanceof GoChannelSend;
 	}
 
-	/*
-	 * 	Global variables. Like general project development using Go, chaincodes developers also consider global variables. As global variables can be changed inherently, the usage of global variables might cause non-determinism
-	 * in chaincode
-	 */
-	private boolean matchGlobalVariable(Statement node) {
-		// TODO 
-		return false;
-	}
+
 
 	/*
-	 * Phantom Read. GetHistoryForKey is a fabric-shim API which returns a history of key values across time. For
-	 * each historic key update, the historic value, associated transaction id, and timestamp are returned. This means
-	 * that data retrieved using this method should not be used to write any new data or update data on the ledger. This
-	 * could lead to unexpected behaviors which may affect the execution of transactions and cause unintended results.
-	 */
-	private boolean matchPhantomRead(UnresolvedCall call) {
-		
-		List<String> OTHER_SIGNATURES = List.of("GetHistoryForKey");
-		
-		if( OTHER_SIGNATURES.contains(call.getTargetName()) 
-				&& call.getParameters().length > 1 
-				&& call.getParameters()[0] instanceof VariableRef) {
-			
-			VariableRef var = (VariableRef) call.getParameters()[0];
-			if(var.getStaticType().isUntyped()) {
-				Untyped type = (Untyped) var.getStaticType();
-				//TODO: with type inference we should check if it is typed as shim.ChaincodeStubInterface
-				return true;			
-			}
-		}
-		
-		return false;
-	}
-
-	/*
-	 * System timestamp. In Go chaincode, timestamp-based libraries leads to inconsistent computation between
+	 * System timestamp. In Go, timestamp-based libraries leads to inconsistent computation between
 	 * peers, leading to a lack of consensus. For example, the time library in Go allows peers to get the current timestamp
 	 * at a given time. It is unlikely that all peers execute a transaction at the same time and receives a similar timestamp.
 	 */
@@ -261,23 +210,10 @@ public class BreakConsensusChaincodeChecker implements SyntacticCheck {
 
 	/*
 	 * Random number generation. Go has built-in support for random number generation in the standard library.
-	 * In Hyperledger Fabric, transactions are simulated by the endorsers. Therefore, use of random number generation
-	 * functions in the chaincodes may lead to inconsistency among the peers
 	 */
 	private boolean matchRandomStatement(UnresolvedCall call) {
-		
-		List<String> RANDOM_SIGNATURES = List.of("Intn", "Float64");
-		
-		if(RANDOM_SIGNATURES.contains(call.getTargetName()) 
-				&& call.getParameters().length > 1 
-				&& call.getParameters()[0] instanceof VariableRef) {
-			
-			VariableRef var = (VariableRef) call.getParameters()[0];
-			if(var.getName().equals("rand"))
-				return true;			
-		}
 
-		return false;
+		return  matchAnyPackageSignatures("math/rand", call, true);
 	}
 
 	@Override
--- go-lisa/src/test/java/NonDetStatementsCheckTest.java
+++ go-lisa/src/test/java/NonDetStatementsCheckTest.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 
 import org.junit.Test;
 
-import it.unive.golisa.analysis.BreakConsensusChaincodeChecker;
+import it.unive.golisa.checker.BreakConsensusGoSmartContractChecker;
 import it.unive.lisa.LiSAConfiguration;
 
 public class NonDetStatementsCheckTest extends GoAnalysisTestExecutor {
 
 	@Test
 	public void testSyntacticChecks() throws IOException{
-		LiSAConfiguration conf = new LiSAConfiguration().setDumpTypeInference(true).setInferTypes(true).addSyntacticCheck(new BreakConsensusChaincodeChecker());
+		LiSAConfiguration conf = new LiSAConfiguration().setDumpTypeInference(true).setInferTypes(true).addSyntacticCheck(new BreakConsensusGoSmartContractChecker());
 		perform("nondet", "nondet.go", conf);
 	}
 }

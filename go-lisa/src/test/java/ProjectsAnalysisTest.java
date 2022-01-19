250ada6e2d662ef4fb16c24b30437c330264a890
VincenzoArceri <vincenzoarceri.92@gmail.com>
2022-01-19 01:36:31 +0100

    project analysis test

---
+++ go-lisa/src/test/java/ProjectsAnalysisTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import it.unive.golisa.cli.MyContextBasedAnalysis;
+import it.unive.lisa.AnalysisSetupException;
+import it.unive.lisa.LiSAConfiguration;
+import it.unive.lisa.LiSAFactory;
+import it.unive.lisa.analysis.AbstractState;
+import it.unive.lisa.analysis.heap.MonolithicHeap;
+import it.unive.lisa.analysis.numeric.Interval;
+import it.unive.lisa.interprocedural.ReturnTopPolicy;
+import it.unive.lisa.interprocedural.callgraph.RTACallGraph;
+
+public class ProjectsAnalysisTest extends GoAnalysisTestExecutor {
+
+	@Test
+	public void testProjectAnalysis001() throws AnalysisSetupException {
+		LiSAConfiguration conf = new LiSAConfiguration()
+				.setAbstractState(LiSAFactory.getDefaultFor(AbstractState.class, new MonolithicHeap(), new Interval()))
+				.setInferTypes(true)
+				.setDumpAnalysis(true)
+				.setCallGraph(new RTACallGraph())
+				.setInterproceduralAnalysis(new MyContextBasedAnalysis<>())
+				.setOpenCallPolicy(ReturnTopPolicy.INSTANCE);
+		perform("go-projects", "a.go", conf);
+	}
+}

e145b2f018d7d90d13e3cd6d09eb30cffbda5a36
VincenzoArceri <vincenzoarceri.92@gmail.com>
2022-06-28 15:41:45 +0200

    Remove Apron testcases

---
+++ go-lisa/boleto.go
@@ -0,0 +1,13 @@
+// from https://www.tutorialspoint.com/go/go_goto_statement.htm
+func m2() {
+   var a int = 10
+
+   LOOP: for a < 20 {
+      if a == 15 {
+         a = a + 1
+         goto LOOP
+      }
+      fmt.Printf("value of a: %d", a)
+      a++     
+   }  
+}

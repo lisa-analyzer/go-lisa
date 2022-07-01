fc0e53648db6ec830983bf40b4a3b47a0084273f
VincenzoArceri <vincenzoarceri.92@gmail.com>
2022-07-01 23:45:44 +0200

    Javadoc

--- go-lisa/src/main/java/it/unive/golisa/loader/Loader.java
+++ go-lisa/src/main/java/it/unive/golisa/loader/Loader.java
@@ -3,7 +3,8 @@ package it.unive.golisa.loader;
 import it.unive.lisa.program.Program;
 
 /**
- * The interface represents a component ables to load information into a program.
+ * The interface represents a component ables to load information into a
+ * program.
  * 
  * @author <a href="mailto:luca.olivieri@univr.it">Luca Olivieri</a>
  */

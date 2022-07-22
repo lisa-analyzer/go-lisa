0d317876ffa0922b530465e803a7e17b5857b131
lucaneg <luca.negrinii@gmail.com>
2022-07-22 17:29:14 +0200

    Automatic regeneration of test file

--- go-lisa/src/test/java/GoAnalysisTestExecutor.java
+++ go-lisa/src/test/java/GoAnalysisTestExecutor.java
@@ -2,22 +2,28 @@ import static it.unive.lisa.outputs.compare.JsonReportComparer.compare;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.apache.commons.io.FileUtils;
-
 import it.unive.golisa.frontend.GoFrontEnd;
 import it.unive.golisa.loader.Loader;
 import it.unive.lisa.AnalysisException;
 import it.unive.lisa.LiSA;
 import it.unive.lisa.LiSAConfiguration;
+import it.unive.lisa.outputs.compare.JsonReportComparer.DiffReporter;
+import it.unive.lisa.outputs.compare.JsonReportComparer.REPORTED_COMPONENT;
+import it.unive.lisa.outputs.compare.JsonReportComparer.REPORT_TYPE;
 import it.unive.lisa.outputs.json.JsonReport;
+import it.unive.lisa.outputs.json.JsonReport.JsonWarning;
 import it.unive.lisa.program.Program;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Collection;
+import java.util.HashSet;
+import org.apache.commons.io.FileUtils;
 
 public abstract class GoAnalysisTestExecutor {
 
@@ -26,16 +32,36 @@ public abstract class GoAnalysisTestExecutor {
 
 	protected void perform(String folder, String source, LiSAConfiguration configuration, Loader... loaders) {
 		System.out.println("Testing " + getCaller());
-		performAux(folder, null, source, configuration, loaders);
+		performAux(folder, null, source, configuration, false, loaders);
 	}
 
 	protected void perform(String folder, String subfolder, String source, LiSAConfiguration configuration,
 			Loader... loaders) {
 		System.out.println("Testing " + getCaller());
-		performAux(folder, subfolder, source, configuration, loaders);
+		performAux(folder, subfolder, source, configuration, false, loaders);
 	}
 
-	private void performAux(String folder, String subfolder, String source, LiSAConfiguration configuration,
+	protected void perform(String folder, String source, LiSAConfiguration configuration, boolean update,
+			Loader... loaders) {
+		System.out.println("Testing " + getCaller());
+		performAux(folder, null, source, configuration, update, loaders);
+	}
+
+	protected void perform(String folder, String subfolder, String source, LiSAConfiguration configuration,
+			boolean update,
+			Loader... loaders) {
+		System.out.println("Testing " + getCaller());
+		performAux(folder, subfolder, source, configuration, update, loaders);
+	}
+
+	protected void performAux(String folder, String subfolder, String source, LiSAConfiguration configuration,
+			Loader... loaders) {
+		System.out.println("Testing " + getCaller());
+		performAux(folder, subfolder, source, configuration, false, loaders);
+	}
+
+	protected void performAux(String folder, String subfolder, String source, LiSAConfiguration configuration,
+			boolean update,
 			Loader... loaders) {
 		Path expectedPath = Paths.get(EXPECTED_RESULTS_DIR, folder);
 		Path actualPath = Paths.get(ACTUAL_RESULTS_DIR, folder);
@@ -83,7 +109,11 @@ public abstract class GoAnalysisTestExecutor {
 		try (FileReader l = new FileReader(expFile); FileReader r = new FileReader(actFile)) {
 			JsonReport expected = JsonReport.read(l);
 			JsonReport actual = JsonReport.read(r);
-			assertTrue("Results are different", compare(expected, actual, expectedPath.toFile(), actualPath.toFile()));
+			Accumulator acc = new Accumulator(expectedPath);
+			boolean compare = compare(expected, actual, expectedPath.toFile(), actualPath.toFile(), acc);
+			if (!compare && update)
+				regen(expectedPath, actualPath, expFile, actFile, acc);
+			assertTrue("Results are different", compare);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace(System.err);
 			fail("Unable to find report file");
@@ -101,5 +131,87 @@ public abstract class GoAnalysisTestExecutor {
 		// 3: it.unive.lisa.test.AnalysisTest.perform()
 		// 4: caller
 		return trace[4].getClassName() + "::" + trace[4].getMethodName();
+	}
+
+	private void regen(Path expectedPath, Path actualPath, File expFile, File actFile, Accumulator acc)
+			throws IOException {
+		boolean updateReport = !acc.addedWarning.isEmpty() || !acc.removedWarning.isEmpty()
+				|| !acc.addedFilePaths.isEmpty() || !acc.removedFilePaths.isEmpty()
+				|| !acc.changedFileName.isEmpty();
+		if (updateReport) {
+			Files.copy(actFile.toPath(), expFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			System.err.println("- Updated report.json");
+		}
+		for (Path f : acc.removedFilePaths) {
+			Files.delete(Paths.get(expectedPath.toString(), f.toString()));
+			System.err.println("- Deleted " + f);
+		}
+		for (Path f : acc.addedFilePaths) {
+			Files.copy(Paths.get(actualPath.toString(), f.toString()),
+					Paths.get(expectedPath.toString(), f.toString()));
+			System.err.println("- Copied (new) " + f);
+		}
+		for (Path f : acc.changedFileName) {
+			Path fresh = Paths.get(expectedPath.toString(), f.toString());
+			Files.copy(
+					Paths.get(actualPath.toString(), f.toString()),
+					fresh,
+					StandardCopyOption.REPLACE_EXISTING);
+			System.err.println("- Copied (update) " + fresh);
+		}
+	}
+
+	private class Accumulator implements DiffReporter {
+
+		private final Collection<Path> changedFileName = new HashSet<>();
+		private final Collection<Path> addedFilePaths = new HashSet<>();
+		private final Collection<Path> removedFilePaths = new HashSet<>();
+		private final Collection<JsonWarning> addedWarning = new HashSet<>();
+		private final Collection<JsonWarning> removedWarning = new HashSet<>();
+		private final Path exp;
+
+		public Accumulator(Path exp) {
+			this.exp = exp;
+		}
+
+		@Override
+		public void report(REPORTED_COMPONENT component, REPORT_TYPE type, Collection<?> reported) {
+			switch (type) {
+			case ONLY_FIRST:
+				switch (component) {
+				case FILES:
+					reported.forEach(e -> removedFilePaths.add(Paths.get((String) e)));
+					break;
+				case WARNINGS:
+					reported.forEach(e -> removedWarning.add((JsonWarning) e));
+					break;
+				default:
+					break;
+				}
+				break;
+			case ONLY_SECOND:
+				switch (component) {
+				case FILES:
+					reported.forEach(e -> addedFilePaths.add(Paths.get((String) e)));
+					break;
+				case WARNINGS:
+					reported.forEach(e -> addedWarning.add((JsonWarning) e));
+					break;
+				default:
+					break;
+				}
+				break;
+			case COMMON:
+			default:
+				break;
+
+			}
+		}
+
+		@Override
+		public void fileDiff(String first, String second, String message) {
+			Path file = Paths.get(first);
+			changedFileName.add(exp.relativize(file));
+		}
 	}
 }

3fc6ff092fe564b2255995d0c6cf5109de6c9755
VincenzoArceri <vincenzoarceri.92@gmail.com>
2022-11-03 16:54:22 +0100

    Refactor instrumentation tests

--- go-lisa/src/test/java/InstrumentationTest.java
+++ go-lisa/src/test/java/InstrumentationTest.java
@@ -1,4 +1,6 @@
 
+import org.junit.Test;
+
 import it.unive.lisa.AnalysisSetupException;
 import it.unive.lisa.LiSAConfiguration;
 import it.unive.lisa.LiSAFactory;
@@ -6,13 +8,15 @@ import it.unive.lisa.analysis.AbstractState;
 import it.unive.lisa.analysis.heap.pointbased.PointBasedHeap;
 import it.unive.lisa.analysis.numeric.Interval;
 import it.unive.lisa.analysis.types.InferredTypes;
-import org.junit.Test;
+import it.unive.lisa.interprocedural.ReturnTopPolicy;
 
 public class InstrumentationTest extends GoAnalysisTestExecutor {
 
 	@Test
 	public void returnStatementInstrumentationTest() throws AnalysisSetupException {
-		LiSAConfiguration conf = new LiSAConfiguration().setSerializeResults(true)
+		LiSAConfiguration conf = new LiSAConfiguration()
+				.setSerializeResults(true)
+				.setOpenCallPolicy(ReturnTopPolicy.INSTANCE)
 				.setAbstractState(LiSAFactory.getDefaultFor(AbstractState.class, new PointBasedHeap(), new Interval(),
 						new InferredTypes()));
 		perform("instrumentation/return-statement", "instrumented-returns.go", conf);

47255f8681249b58e7044a5b189a759b6bb966cf
VincenzoArceri <vincenzoarceri.92@gmail.com>
2022-12-17 15:19:00 +0100

    Fix black identifier tests

--- go-lisa/src/test/java/BlankIdentifierTest.java
+++ go-lisa/src/test/java/BlankIdentifierTest.java
@@ -1,15 +1,16 @@
 
+import org.junit.Test;
+
 import it.unive.golisa.analysis.heap.GoAbstractState;
 import it.unive.golisa.analysis.heap.GoFieldSensitivePointBasedHeap;
 import it.unive.lisa.AnalysisSetupException;
 import it.unive.lisa.LiSAConfiguration;
-import it.unive.lisa.LiSAFactory;
+import it.unive.lisa.analysis.nonrelational.value.TypeEnvironment;
 import it.unive.lisa.analysis.nonrelational.value.ValueEnvironment;
 import it.unive.lisa.analysis.numeric.Interval;
-import it.unive.lisa.analysis.value.TypeDomain;
+import it.unive.lisa.analysis.types.InferredTypes;
 import it.unive.lisa.interprocedural.ContextBasedAnalysis;
 import it.unive.lisa.interprocedural.callgraph.RTACallGraph;
-import org.junit.Test;
 
 public class BlankIdentifierTest extends GoAnalysisTestExecutor {
 
@@ -18,7 +19,7 @@ public class BlankIdentifierTest extends GoAnalysisTestExecutor {
 		LiSAConfiguration conf = new LiSAConfiguration();
 		conf.abstractState = new GoAbstractState<>(new GoFieldSensitivePointBasedHeap(),
 				new ValueEnvironment<>(new Interval()),
-				LiSAFactory.getDefaultFor(TypeDomain.class));
+				new TypeEnvironment<>(new InferredTypes()));
 		conf.serializeResults = true;
 		conf.callGraph = new RTACallGraph();
 		conf.interproceduralAnalysis = new ContextBasedAnalysis<>();

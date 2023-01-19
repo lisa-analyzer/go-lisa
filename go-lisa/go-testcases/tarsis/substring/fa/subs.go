99ce54ddc8ade8fd23a537861de7a40f820ff3c3
VincenzoArceri <vincenzoarceri.92@gmail.com>
2023-01-19 15:28:02 +0100

    Missing files

--- go-lisa/go-testcases/tarsis/substring/fa/subs.go
+++ go-lisa/go-testcases/tarsis/substring/fa/subs.go
@@ -1,12 +1,12 @@
 package main
 
 func main(nondet bool) {
-	res := "substring test"
+	res := "test"
 	if (nondet) {
 		res = res + " passed"
 	} else {
 		res = res + " failed"
 	}
 
-	res = res[5:18]
+	res = res[2:10]
 }

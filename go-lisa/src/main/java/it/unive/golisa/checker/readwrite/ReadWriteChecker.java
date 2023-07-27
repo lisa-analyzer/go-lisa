56a283ac6e8ac8a0984f3fa6c355b63403744a5e
Luca Olivieri <oli.luca93@gmail.com>
2023-07-27 19:48:20 +0200

    Implementing ReadWriteChecker

--- go-lisa/src/main/java/it/unive/golisa/checker/readwrite/ReadWriteChecker.java
+++ go-lisa/src/main/java/it/unive/golisa/checker/readwrite/ReadWriteChecker.java
@@ -1,12 +1,16 @@
 package it.unive.golisa.checker.readwrite;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.commons.lang3.tuple.Triple;
 
 import it.unive.golisa.checker.readwrite.ReadWriteHFUtils.KeyType;
 import it.unive.golisa.checker.readwrite.ReadWriteHFUtils.TypeInstruction;
+import it.unive.lisa.analysis.AnalysisState;
 import it.unive.lisa.analysis.AnalyzedCFG;
 import it.unive.lisa.analysis.SemanticException;
 import it.unive.lisa.analysis.SimpleAbstractState;
@@ -28,6 +32,8 @@ import it.unive.lisa.program.cfg.statement.call.CFGCall;
 import it.unive.lisa.program.cfg.statement.call.Call;
 import it.unive.lisa.program.cfg.statement.call.NativeCall;
 import it.unive.lisa.program.cfg.statement.call.UnresolvedCall;
+import it.unive.lisa.symbolic.SymbolicExpression;
+import it.unive.lisa.symbolic.value.ValueExpression;
 
 
 
@@ -84,29 +90,34 @@ public class ReadWriteChecker implements
 		if(ReadWriteHFUtils.isReadOrWriteCall(call)) {
 			try {
 				Pair<TypeInstruction, Triple<String, KeyType, int[]>> info = ReadWriteHFUtils.getReadWriteInfo(call);
+				int[] keyParams = info.getRight().getRight();
 				
+				Collection<AnalyzedCFG<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Tarsis>, TypeEnvironment<InferredTypes>>, PointBasedHeap, ValueEnvironment<Tarsis>, TypeEnvironment<InferredTypes>>> ss = tool.getResultOf(call.getCFG());
 				for (AnalyzedCFG<
 						SimpleAbstractState<PointBasedHeap, ValueEnvironment<Tarsis>, TypeEnvironment<InferredTypes>>,
 						PointBasedHeap, ValueEnvironment<Tarsis>,
 						TypeEnvironment<InferredTypes>> result : tool.getResultOf(call.getCFG())) {
 					Call resolved = (Call) tool.getResolvedVersion(call, result);
+					ArrayList<Set<Tarsis>> keyValues = new ArrayList<>();
+					
 					if (resolved instanceof NativeCall) {
 						NativeCall nativeCfg = (NativeCall) resolved;
 						Collection<CodeMember> nativeCfgs = nativeCfg.getTargets();
 						for (CodeMember n : nativeCfgs) {
 							Parameter[] parameters = n.getDescriptor().getFormals();
-							Object[] valStringDomain = new Object[info.getRight().getRight().length];
-							// TODO
-
+							keyValues = extractKeyValues(call, keyParams, parameters, node, result);
 						}
 					} else if (resolved instanceof CFGCall) {
 						CFGCall cfg = (CFGCall) resolved;
+						
 						for (CodeMember n : cfg.getTargets()) {
 							Parameter[] parameters = n.getDescriptor().getFormals();
-							Object[] valStringDomain = new Object[info.getRight().getRight().length];
-							// TODO
+							keyValues = extractKeyValues(call, keyParams, parameters, node, result);
 						}
 					}
+					
+					FactoryReadWriteHF.buildReadWriteHF(info, keyValues);
+					
 				}
 			} catch (SemanticException e) {
 				System.err.println("Cannot check " + node);
@@ -118,6 +129,31 @@ public class ReadWriteChecker implements
 	}
 
 
+
+	private ArrayList<Set<Tarsis>> extractKeyValues(UnresolvedCall call, int[] keyParams, Parameter[] parameters, Statement node, AnalyzedCFG<SimpleAbstractState<PointBasedHeap, ValueEnvironment<Tarsis>, TypeEnvironment<InferredTypes>>, PointBasedHeap, ValueEnvironment<Tarsis>, TypeEnvironment<InferredTypes>> result) throws SemanticException {
+		
+		ArrayList<Set<Tarsis>> valStringDomain = new ArrayList<>(keyParams.length);
+		
+		for(int i=0; i < keyParams.length; i++) {
+			int par = keyParams[i];
+			valStringDomain.add(new HashSet<>());
+			if(par < parameters.length) {
+				
+				AnalysisState<
+				SimpleAbstractState<PointBasedHeap, ValueEnvironment<Tarsis>,
+						TypeEnvironment<InferredTypes>>,
+				PointBasedHeap, ValueEnvironment<Tarsis>,
+				TypeEnvironment<InferredTypes>> state = result
+						.getAnalysisStateAfter(call.getParameters()[par]);
+				for (SymbolicExpression stack : state.rewrite(state.getComputedExpressions(), node)) {
+					valStringDomain.get(i).add(state.getState().getValueState().eval((ValueExpression) stack, node));
+				}
+			}
+		}
+		
+		return valStringDomain;
+		
+	}
 
 	@Override
 	public boolean visit(

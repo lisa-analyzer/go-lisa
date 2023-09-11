09a7345e8855504ccc8cdeb98a7ad46258948f40
VincenzoArceri <vincenzoarceri.92@gmail.com>
2023-09-11 09:16:55 +0200

    Fixing abstract semantics of GetStringArgs

--- go-lisa/src/main/java/it/unive/golisa/cfg/runtime/shim/method/GetStringArgs.java
+++ go-lisa/src/main/java/it/unive/golisa/cfg/runtime/shim/method/GetStringArgs.java
@@ -1,9 +1,9 @@
 package it.unive.golisa.cfg.runtime.shim.method;
 
-import it.unive.golisa.analysis.taint.Clean;
+import it.unive.golisa.analysis.taint.TaintDomain;
 import it.unive.golisa.cfg.runtime.shim.type.ChaincodeStub;
-import it.unive.golisa.cfg.type.GoStringType;
 import it.unive.golisa.cfg.type.composite.GoSliceType;
+import it.unive.golisa.cfg.type.numeric.signed.GoIntType;
 import it.unive.lisa.analysis.AbstractState;
 import it.unive.lisa.analysis.AnalysisState;
 import it.unive.lisa.analysis.SemanticException;
@@ -14,6 +14,7 @@ import it.unive.lisa.analysis.value.TypeDomain;
 import it.unive.lisa.analysis.value.ValueDomain;
 import it.unive.lisa.interprocedural.InterproceduralAnalysis;
 import it.unive.lisa.program.CompilationUnit;
+import it.unive.lisa.program.annotations.Annotations;
 import it.unive.lisa.program.cfg.CFG;
 import it.unive.lisa.program.cfg.CodeLocation;
 import it.unive.lisa.program.cfg.CodeMemberDescriptor;
@@ -24,14 +25,25 @@ import it.unive.lisa.program.cfg.statement.NaryExpression;
 import it.unive.lisa.program.cfg.statement.PluggableStatement;
 import it.unive.lisa.program.cfg.statement.Statement;
 import it.unive.lisa.symbolic.SymbolicExpression;
+import it.unive.lisa.symbolic.heap.AccessChild;
+import it.unive.lisa.symbolic.heap.HeapDereference;
+import it.unive.lisa.symbolic.heap.HeapReference;
+import it.unive.lisa.symbolic.heap.MemoryAllocation;
+import it.unive.lisa.symbolic.value.PushAny;
+import it.unive.lisa.symbolic.value.Variable;
+import it.unive.lisa.type.ReferenceType;
+import it.unive.lisa.type.Type;
+import it.unive.lisa.type.Untyped;
 
 /**
  * func (s *ChaincodeStub) GetStringArgs() []string.
- * https://pkg.go.dev/github.com/hyperledger/fabric-chaincode-go/shim#ChaincodeStub.GetStringArgs
  * 
+ * @link https://pkg.go.dev/github.com/hyperledger/fabric-chaincode-go/shim#ChaincodeStub.GetStringArgs
  * @author <a href="mailto:luca.olivieri@univr.it">Luca Olivieri</a>
  */
 public class GetStringArgs extends NativeCFG {
+
+	private final static Annotations anns = new Annotations(TaintDomain.CLEAN_ANNOTATION);
 
 	/**
 	 * Builds the native cfg.
@@ -41,7 +53,7 @@ public class GetStringArgs extends NativeCFG {
 	 */
 	public GetStringArgs(CodeLocation location, CompilationUnit shimUnit) {
 		super(new CodeMemberDescriptor(location, shimUnit, true, "GetStringArgs",
-				GoSliceType.getSliceOfStrings(),
+				GoSliceType.getSliceOfStrings(), anns,
 				new Parameter(location, "s", ChaincodeStub.getChaincodeStubType(shimUnit.getProgram()))),
 				GetStringArgsImpl.class);
 	}
@@ -52,7 +64,7 @@ public class GetStringArgs extends NativeCFG {
 	 * @author <a href="mailto:vincenzo.arceri@unipr.it">Vincenzo Arceri</a>
 	 */
 	public static class GetStringArgsImpl extends NaryExpression
-			implements PluggableStatement {
+	implements PluggableStatement {
 
 		private Statement original;
 
@@ -84,18 +96,47 @@ public class GetStringArgs extends NativeCFG {
 		 * @param params   the parameters
 		 */
 		public GetStringArgsImpl(CFG cfg, CodeLocation location, Expression... params) {
-			super(cfg, location, "GetStringArgsImpl", GoSliceType.lookup(GoStringType.INSTANCE), params);
+			super(cfg, location, "GetStringArgsImpl", GoSliceType.getSliceOfStrings(), params);
 		}
 
 		@Override
 		public <A extends AbstractState<A, H, V, T>,
-				H extends HeapDomain<H>,
-				V extends ValueDomain<V>,
-				T extends TypeDomain<T>> AnalysisState<A, H, V, T> expressionSemantics(
-						InterproceduralAnalysis<A, H, V, T> interprocedural, AnalysisState<A, H, V, T> state,
-						ExpressionSet<SymbolicExpression>[] params, StatementStore<A, H, V, T> expressions)
+		H extends HeapDomain<H>,
+		V extends ValueDomain<V>,
+		T extends TypeDomain<T>> AnalysisState<A, H, V, T> expressionSemantics(
+				InterproceduralAnalysis<A, H, V, T> interprocedural, AnalysisState<A, H, V, T> state,
+				ExpressionSet<SymbolicExpression>[] params, StatementStore<A, H, V, T> expressions)
 						throws SemanticException {
-			return state.smallStepSemantics(new Clean(GoSliceType.getSliceOfStrings(), getLocation()), original);
+			Type sliceOfString = GoSliceType.getSliceOfStrings();
+
+			// Allocates the new heap allocation
+			MemoryAllocation created = new MemoryAllocation(sliceOfString, getLocation(), anns, true);
+			AnalysisState<A, H, V, T> allocState = state.smallStepSemantics(created, this);
+
+			AnalysisState<A, H, V, T> result = state.bottom();
+			for (SymbolicExpression allocId : allocState.getComputedExpressions()) {
+				HeapReference ref = new HeapReference(new ReferenceType(sliceOfString), allocId, getLocation());
+				HeapDereference deref = new HeapDereference(sliceOfString, ref,  getLocation());
+
+				// Assign the len property to this hid
+				Variable len = new Variable(Untyped.INSTANCE, "len",
+						getLocation());
+				AccessChild lenAccess = new AccessChild(GoIntType.INSTANCE, deref,
+						len, getLocation());
+				AnalysisState<A, H, V, T> lenState = allocState.assign(lenAccess, new PushAny(GoIntType.INSTANCE, getLocation()), this);
+
+				// Assign the cap property to this hid
+				Variable cap = new Variable(Untyped.INSTANCE, "cap",
+						getLocation());
+				AccessChild capAccess = new AccessChild(GoIntType.INSTANCE, deref,
+						cap, getLocation());
+				AnalysisState<A, H, V, T> capState = lenState.assign(capAccess, new PushAny(GoIntType.INSTANCE, getLocation()), this);
+
+				result = result.lub(capState.smallStepSemantics(ref, original));
+			}
+
+			return result;
+
 		}
 	}
 }

3bd23740c58765da79538f0fdec0fb32cb688fd1
olivieriluca <oli.luca93@gmail.com>
2025-09-28 11:37:51 +0200

    Test clean up and baseline regeneration

--- go-lisa/src/test/java/UnhandledErrorsTest.java
+++ go-lisa/src/test/java/UnhandledErrorsTest.java
@@ -5,14 +5,15 @@ import org.junit.Test;
 
 public class UnhandledErrorsTest extends GoChaincodeTestExecutor {
 
+
 	@Test
 	public void testUnhandledErrors() throws AnalysisSetupException {
 		CronConfiguration conf = new CronConfiguration();
 		conf.syntacticChecks.add(new UnhandledErrorsChecker());
 		conf.jsonOutput = true;
 		conf.compareWithOptimization = false;
-		conf.testDir = "unhandled-errors";
-		conf.programFile = "unhandled-errors.go";
+		conf.testDir = "unhandled-errors/hf-unhandled-errors";
+		conf.programFile = "hf-unhandled-errors.go";
 		perform(conf);
 	}
 }
